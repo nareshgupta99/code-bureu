b90f06b12842eff29366f463623b4cf5efb65bcb
Naresh Gupta <nareshgupta0899@gmail.com>
2024-04-24 14:52:10 +0530

    armstrong

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String s=""+n;
+		int sum=0;
+		while(n!=0){
+			int rem=n%10;
+			rem=(int)Math.pow(rem,s.length());
+			sum=sum+rem;
+			n=n/10;
+		}
+		if(sum==Integer.parseInt(s))
+			System.out.println(true);
+		else
+			System.out.println(false);
+
+	}
+}
